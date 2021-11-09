58756b85a8718b381c2e461cbd556d1d36445bfb
marion <marion.deville-cavellin@hotmail.com>
2021-11-09 15:12:10 +0100

    update example

--- R/population.R
+++ R/population.R
@@ -13,8 +13,10 @@
 #' Une liste structuré comprenant les paramètres de la population mentionnés ci-dessus
 #' @export
 #'
-#' @examples
+#' @examples population(34, 50, 0.8, 500)
 population <- function(identifiant, taille_ini, taux_rep, capacite){
+  if(taux_rep >1){stop("Break reprod > 1")}
+
   structure(list(identifiant = identifiant,
                  capacite = capacite,
                  taux = taux_rep,
