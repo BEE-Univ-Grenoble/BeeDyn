e60a82a851074ad9ca9cddcbf49ac20858a8a506
SolPraseo <titouan.tcheng@gmail.com>
2021-11-09 15:03:20 +0100

    Adds roxygen

--- R/population.R
+++ R/population.R
@@ -1,4 +1,19 @@
 
+#' Title
+#'
+#' @param identifiant
+#' Valeur numérique identifiant l'espèce
+#' @param taille_ini
+#' Taille initiale de la population en question
+#' @param taux_rep
+#' Taux de reproduction maximum de la population
+#' @param capacite
+#' Capacite biotique du milieu
+#' @return
+#' Une liste structuré comprenant les paramètres de la population mentionnés ci-dessus
+#' @export
+#'
+#' @examples
 population <- function(identifiant, taille_ini, taux_rep, capacite){
   structure(list(identifiant = identifiant,
                  capacite = capacite,
