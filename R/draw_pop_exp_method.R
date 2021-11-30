20ba3b4a9685a158e4ed4af21c26cfec52bd9031
Resos-Cam <camille.voisin@univ-grenoble-alpes.fr>
2021-11-30 10:19:01 +0100

    fonction draw_espece

--- R/draw_pop_exp_method.R
+++ R/draw_pop_exp_method.R
@@ -1,6 +1,10 @@
-#' Title
+#' @import ggplot2
+NULL
+
+#' Fonction graphique suivant l'instance de l'objet
+#' Trace l'evolution des populations en fonction du temps
 #'
-#' @param object
+#' @param object Un object du package BeeDyn
 #' @param ylog
 #'
 #' @return
@@ -9,4 +13,25 @@
 #' @examples
 draw_pop_expansion <- function(object, ylog=FALSE) {
    UseMethod("draw_pop_expansion", object)
+}
+
+
+#' Plot l'évolution d'une espece et de ses populations
+#'
+#' @param e Une instance d'espece
+#' @param ylog Axe y en log
+#'
+#' @return Un graph ggplot
+#' @export
+#'
+#' @examples
+draw_pop_expansion.Espece <- function(objet, ylog = FALSE) {
+  gg_espece <- ggplot(data = as_tibble(objet),
+                      aes(x = temps, y = valeur)) +
+    geom_point(aes(col = population, shape = espece)) +
+    geom_line(aes(col = population, lty = espece))
+  if (ylog) {
+    gg_espece <- gg_espece + scale_y_log10()
+  }
+  gg_espece
 }
